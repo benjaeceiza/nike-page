54d5a1de196096472682a5dbcb589c4503f08930
Benjamin Eceiza <benjaceiza400@gmail.com>
2024-07-08 19:31:48 -0300

    .

---
+++ .js/barra.js
@@ -0,0 +1,22 @@
+
+
+const botonMenu = document.getElementById("boton-menu");
+
+botonMenu.onclick = () => alert("clickeaste");
+
+const contenedorMenu = document.createElement("div");
+const listaMenu = document.createElement("ul");
+const itemMenu1 = document.createElement("li");
+const itemMenu2 = document.createElement("li");
+const itemMenu3 = document.createElement("li");
+const itemMenu4 = document.createElement("li");
+
+const linkItem1 = document.createElement("a");
+const linkItem2 = document.createElement("a");
+const linkItem3 = document.createElement("a");
+const linkItem4 = document.createElement("a");
+
+const imagenCarrito = document.createElement("img");
+const linkImagen = document.createElement("a");
+
+
